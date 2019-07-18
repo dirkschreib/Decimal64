c35e3abe7be4e3b93a3233521fa643250fd22ae9
Dirk <dirk.schreib@me.com>
2019-07-18 19:00:05 +0200

    Initial Commit

---
+++ Decimal64/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Decimal64
+//
+//  Created by Dirk on 18.07.19.
+//  Copyright © 2019 Dirk Schreib. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
